36dd0bcdf9fa559ffaa91ddcc99dea68f870e7f1
TheSuguser <joeyzhengzhe@gmail.com>
2017-10-30 22:51:45 +0000

    update

--- JelinekMercerLM.py
+++ JelinekMercerLM.py
@@ -7,6 +7,7 @@ import statnlpbook.ohhla as ohhla
 import math
 import numpy as np
 import matplotlib.pyplot as plt
+import collections
 
 #! SETUP 2
 _snlp_train_dir = _snlp_book_dir + "/data/ohhla/train"
@@ -15,34 +16,39 @@ _snlp_train_song_words = ohhla.words(ohhla.load_all_songs(_snlp_train_dir))
 _snlp_dev_song_words = ohhla.words(ohhla.load_all_songs(_snlp_dev_dir))
 assert(len(_snlp_train_song_words)==1041496)
 
+class JelinekMercerLM(lm.LanguageModel):
+	def __init__(self, unigram, alpha1, bigram, alpha2, trigram, alpha3, fourgram):
+		super().__init__(unigram.vocab, fourgram.order)
+		self.unigram = unigram
+		self.bigram = bigram
+		self.trigram  = trigram
+		self.fourgram = fourgram
+		self.alpha1 = alpha1
+		self.alpha2 = alpha2
+		self.alpha3 = alpha3
+		self.alpha4 = 1 - (alpha1 + alpha2 + alpha3)
 
-# Define Interpolated language model
-class InterpolatedLM(lm.LanguageModel):
-    def __init__(self, main, backoff, alpha):
-        super().__init__(main.vocab, main.order)
-        self.main = main
-        self.backoff = backoff
-        self.alpha = alpha
-    def probability(self, word, *history):
-        return self.alpha * self.main.probability(word,*history) + \
-               (1.0 - self.alpha) * self.backoff.probability(word,*history)
+	def probability(self, word, *history):
 
+		return self.alpha1 * self.unigram.probability(word, *history) + \
+			   self.alpha2 * self.bigram.probability(word, * history) + \
+			   self.alpha3 * self.trigram.probability(word, *history) + \
+			   self.alpha4 * self.fourgram.probability(word, *history) 
 
-
-# Deal with raw data
 oov_train = lm.inject_OOVs(_snlp_train_song_words)
-oov_dev = lm.inject_OOVs(_snlp_train_song_words)
-bigram = lm.NGramLM(oov_dev, 2)
-unigram = lm.NGramLM(oov_dev,1)
-trigram = lm.NGramLM(oov_dev,3)
-fourgram = lm.NGramLM(oov_dev,4)
-inter_lm = InterpolatedLM(bigram, unigram, 0.7)
-new_inter_lm = InterpolatedLM(trigram, inter_lm, 0.2)
-new_new_inter_lm = InterpolatedLM(fourgram, new_inter_lm, 0.1)
-oov_vocab = set(oov_dev)
+#oov_dev = lm.inject_OOVs(_snlp_train_song_words)
+bigram = lm.NGramLM(oov_train, 2)
+unigram = lm.NGramLM(oov_train,1)
+trigram = lm.NGramLM(oov_train,3)
+fourgram = lm.NGramLM(oov_train,4)
+my_LM = JelinekMercerLM(unigram, 0.245, bigram, 0.505, trigram, 0.15, fourgram)
+oov_vocab = set(oov_train)
+vocab = set(_snlp_train_song_words) | set(_snlp_dev_song_words)
 
 
-#Create Language model
+
+
+
 def create_lm(vocab):
     """
     Return an instance of `lm.LanguageModel` defined over the given vocabulary.
@@ -53,8 +59,7 @@ def create_lm(vocab):
     """
     
     
-    return lm.OOVAwareLM(new_new_inter_lm, vocab - oov_vocab)
-
+    return lm.OOVAwareLM(my_LM, vocab - oov_vocab)
 
 #! SETUP 3
 _snlp_test_dir = _snlp_book_dir + "/data/ohhla/dev"
@@ -68,6 +73,8 @@ _snlp_vocab = _snlp_test_vocab | _snlp_train_vocab | _snlp_dev_vocab
 _snlp_lm = create_lm(_snlp_vocab)
 
 
+
+
 #! ASSESSMENT 1
 _snlp_test_token_indices = [100, 1000, 10000]
 _eps = 0.000001
@@ -79,29 +86,3 @@ for i in _snlp_test_token_indices:
 
 #! ASSESSMENT 2
 print("Perlexity:", lm.perplexity(_snlp_lm, _snlp_test_song_words))
-
-
-"""
-# Test parameter
-step = 10
-alpha1_range = np.linspace(0, 1, step)
-alpha2_range = alpha1_range
-
-perp = np.zeros([step, step])
-
-for i in range(step):
-    for j in range(step):
-        print(i,j)
-
-        inter_lm = InterpolatedLM(bigram, unigram, alpha1_range[i])
-        new_inter_lm = InterpolatedLM(trigram, inter_lm, alpha2_range[j])
-        _snlp_lm = create_lm(_snlp_test_vocab)
-        perp[i,j] = lm.perplexity(_snlp_lm, _snlp_test_song_words)
-
-
-
-plt.figure()
-plt.imshow(perp, extent = [0,1,0,1], cmap = "hot")
-plt.show()
-
-"""
